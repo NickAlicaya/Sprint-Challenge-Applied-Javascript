dbc07433748bbdfd7efb0503beb194c224a5c60e
nicanor-alicaya <donalicaya@gmail.com>
2019-11-01 13:22:20 -0400

    MVP accomplished

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,56 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+function cardMaker(data){
+//elements    
+const card = document.createElement('div');
+const headline = document.createElement('div');
+const author = document.createElement('div');
+const imgContainer = document.createElement('div');
+const img = document.createElement('img');
+const authorsName = document.createElement('span');
+
+//classes
+card.classList.add('card');
+headline.classList.add('headline');
+author.classList.add('author');
+imgContainer.classList.add('img-container');
+
+//content
+headline.textContent= data.headline;
+img.src = data.authorPhoto;
+authorsName.textContent = data.authorName;
+
+
+//structure
+cardsContainer.appendChild(card)
+card.appendChild(headline);
+card.appendChild(author);
+author.appendChild(imgContainer);
+imgContainer.appendChild(img);
+author.appendChild(authorsName);
+
+return card;
+}
+const cardsContainer = document.querySelector('.cards-container');
+
+
+axios
+.get('https://lambda-times-backend.herokuapp.com/articles')
+.then((response) => {
+    console.log(response.data.articles);
+    response.data.articles.bootstrap.forEach(article =>  
+        cardMaker(article));
+    response.data.articles.javascript.forEach(article =>  
+        cardMaker(article));   
+    response.data.articles.jquery.forEach(article =>  
+        cardMaker(article)); 
+    response.data.articles.node.forEach(article => 
+        cardMaker(article));
+    response.data.articles.technology.forEach(article => 
+        cardMaker(article));
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
